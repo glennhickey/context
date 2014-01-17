439bf6c6d032bc62fd0716bf3b85f73d6cf1abda
Glenn Hickey <glenn.hickey@gmail.com>
2014-01-17 17:49:00 -0500

    add ftagscore to build

---
+++ src/ftagscore.cpp
@@ -0,0 +1,20 @@
+//Copyright (C) 2010 by Glenn Hickey
+//
+//Released under the MIT license, see LICENSE.txt
+
+#include <cassert>
+
+#include "ftagscore.h"
+
+using namespace std;
+using namespace FTAGStates;
+
+FTAGScore::FTAGScore() 
+{
+
+}
+
+FTAGScore::~FTAGScore()
+{
+
+}
---
+++ src/ftagscore.h
@@ -0,0 +1,25 @@
+//Copyright (C) 2010 by Glenn Hickey
+//
+//Released under the MIT license, see LICENSE.txt
+
+#ifndef _FTAGSCORE_H
+#define _FTAGSCORE_H
+
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <deque>
+
+#include "ftagmodel.h"
+
+class FTAGScore
+{
+public:
+   FTAGScore();
+   virtual ~FTAGScore();
+
+protected:
+
+};
+
+#endif
