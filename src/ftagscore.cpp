ee22b853e738282c1590be488942a2850aefe297
Glenn Hickey <glenn.hickey@gmail.com>
2014-01-19 15:07:40 -0500

    implement fast viterbi score of fixed alignment

--- src/ftagscore.cpp
+++ src/ftagscore.cpp
@@ -3,18 +3,187 @@
 //Released under the MIT license, see LICENSE.txt
 
 #include <cassert>
-
+#include <cstdlib>
+#include <cmath>
+#include <limits>
 #include "ftagscore.h"
 
 using namespace std;
 using namespace FTAGStates;
 
-FTAGScore::FTAGScore() 
-{
+static const double NEGINF = -numeric_limits<double>::max();
 
+
+FTAGScore::FTAGScore(const FTAGModel* model) 
+{
+  if (model != NULL)
+  {
+    setModel(*model);
+  }
+  else
+  {
+    _model = NULL;
+  }
 }
 
 FTAGScore::~FTAGScore()
 {
 
 }
+
+void FTAGScore::setModel(const FTAGModel& model) 
+{
+  _model = &model;
+  _transModel = &_model->getTransitionModel();
+  _emModel = &_model->getEmissionModel();
+}
+
+double FTAGScore::logViterbiScore(const pair<string, string>& alignment)
+{
+  assert(_model != NULL && _transModel != NULL && _emModel != NULL);
+  _alignment = &alignment;
+  assert(_alignment->first.length() == _alignment->second.length());
+  _N = _alignment->first.length();
+
+  // init the DP table
+  init2(_table, 0., 0.);
+  init2(_lastTable, 0., 0.);
+  init2(_lastState, S_Start, S_Start);
+
+  for (size_t pos = 0; pos < _N; ++pos)
+  {
+    _state[0] = getColID(pos);
+    _state[1] = getContext(_state[0]);
+    if (_state[0] == S_Mx)
+    {
+      scoreMatch(pos);
+    }
+    else if (_state[0] == S_Ix)
+    {
+      scoreIndel(pos);
+      // skip ahead
+      pos += _indelSize - 1; 
+    }
+    copy2(_lastState, _state);
+    copy2(_lastTable, _table);
+  }
+
+  _table[0] = _lastTable[0] + log(_transModel->prTrans(_lastState[0], S_End));
+  _table[1] = _lastTable[1] + log(_transModel->prTrans(_lastState[1], S_End));
+  return std::max(_table[0], _table[1]);
+}
+
+void FTAGScore::scoreMatch(size_t pos)
+{
+  DNA c1 = DNASubModel::charToDNA(_alignment->first[pos]);
+  DNA c2 = DNASubModel::charToDNA(_alignment->second[pos]);
+  // emission probability
+  double emProb = log(_emModel->prFullM(c1, c2));
+  
+  // DP step
+  double tPr0 = _lastTable[0] + log(_transModel->prTrans(_lastState[0], S_Mx));
+  double tPr1 = tPr0;
+  if (_lastState[0] != S_Mx)
+  { 
+    tPr1 = _lastTable[1] + log(_transModel->prTrans(_lastState[1], S_Mx));
+  }
+  _table[0] = emProb + max(tPr0, tPr1);
+  _table[1] = _table[0];
+}
+
+void FTAGScore::scoreIndel(size_t pos)
+{
+  // compute score of vanilla indel
+  double insScore = 0.0;
+  _indelSize = 0;
+  for (size_t i = pos; i < _N && getColID(i) == _state[0]; ++i, ++_indelSize)
+  {
+    // compute emission log prob
+    if (_state[0] == S_Ix)
+    {
+      DNA c = DNASubModel::charToDNA(_alignment->second[i]);
+      insScore += log(_emModel->prFullI(c));
+    }
+    else
+    {
+      assert(_state[0] == S_Dx);
+      DNA c = DNASubModel::charToDNA(_alignment->first[i]);
+      insScore += log(_emModel->prFullD(c));
+    } 
+    // add transition log prob
+    if (i > pos)
+    {
+      insScore += log(_transModel->prTrans(_state[0], _state[0]));
+    }
+  }
+  // DP step.
+  double tPr0 = _lastTable[0] + log(_transModel->prTrans(_lastState[0], 
+                                                         _state[0]));
+  double tPr1 = _lastTable[1] + log(_transModel->prTrans(_lastState[1],
+                                                         _state[0]));
+  _table[0] = insScore + max(tPr0, tPr1);
+
+
+  // compute score of contexst indel;
+  _table[1] = NEGINF;
+  if (pos >= _indelSize)
+  {
+    State inside = _state[0] == S_MIx ? S_MIzM : S_MDyM;
+    insScore = 0.0;
+    for (size_t i = pos; i < _N && getColID(i) == _state[0]; ++i, ++_indelSize)
+    {
+      DNA c1 = DNASubModel::charToDNA(_alignment->first[i - _indelSize]);
+      DNA c2 = DNASubModel::charToDNA(_alignment->second[i - _indelSize]);
+      // compute emission log prob
+      if (_state[0] == S_Ix)
+      {
+        DNA c3 = DNASubModel::charToDNA(_alignment->second[i]);
+        insScore += log(_emModel->prFullMI(c1, c2, c3));
+      }
+      else
+      {
+        DNA c3 = DNASubModel::charToDNA(_alignment->first[i]);
+        insScore += log(_emModel->prFullMD(c1, c2, c3));        
+      }
+      // compute transition log prob
+      if (i == pos + 1)
+      {
+        insScore += log(_transModel->prTrans(_state[1], inside));
+      }
+      else if (i > pos + 1)
+      {
+        insScore += log(_transModel->prTrans(inside, inside));
+      }
+    }
+    // close the context indel
+    State eState = _indelSize > 1 ? inside : _state[1];
+    insScore += log(_transModel->prTrans(eState, S_Close));
+
+    // DP step
+    double tPr0 = _lastTable[0] + log(_transModel->prTrans(_lastState[0], 
+                                                           _state[1]));
+    double tPr1 = _lastTable[1] + log(_transModel->prTrans(_lastState[1], 
+                                                           _state[1]));
+    _table[1] = insScore + max(tPr0, tPr1);
+  }
+}
+
+State FTAGScore::getColID(size_t pos)
+{
+  bool topGap = _alignment->first[pos] == '-';
+  bool botGap = _alignment->second[pos] == '-';
+  assert(!topGap || !botGap);
+  if (topGap == false and botGap == false)
+  {
+    return S_Mx;
+  }
+  else if (topGap == true)
+  {
+    return S_Ix;
+  }
+  else
+  {
+    return S_Dx;
+  }
+}
+
--- src/ftagscore.h
+++ src/ftagscore.h
@@ -12,14 +12,104 @@
 
 #include "ftagmodel.h"
 
+/** This is a class that handles core functionality related to scoring a fixed 
+alignment.  IE, the alignment is already known, and we just want to scan it
+over to see what it's viterbi score is */
+
 class FTAGScore
 {
 public:
-   FTAGScore();
+   FTAGScore(const FTAGModel* model = NULL);
    virtual ~FTAGScore();
+
+   // model must be set before anything gets done
+   void setModel(const FTAGModel& model);
+   
+   // return the (viterbi) log score of a pairwise alignment. performed in 
+   // linear time because each indel can be resolved independently and is 
+   // either context or not. note that the alignment structure here can 
+   // be obtained from an AXTPair object
+   double logViterbiScore(const std::pair<std::string, std::string>& alignment);
 
 protected:
 
+   // compute score of a single match column (note no transition taken into
+   // account
+   void scoreMatch(size_t pos);
+
+   // given the first position of a gap in the indel, scan right computing the
+   // score
+   void scoreIndel(size_t pos);
+
+   // get the id of a single column.  Either a match, insert, or delete.  Note
+   // because we are just looking at one column, we don't consider context
+   // events -- just want to know if / where the gap is. 
+   FTAGStates::State getColID(size_t pos);
+
+   // convenience for filling out context tables below.  want to be able to
+   // quickly get context version of a state if present;
+   FTAGStates::State getContext(FTAGStates::State state) const;
+
+   // convenience for copying arrays of size two which get used a fair amount
+   template<typename T> void copy2(T a[2], T b[2]) const;
+   template<typename T> void init2(T a[2], T b0, T b1) const;
+   
+protected:
+
+   const FTAGModel* _model;
+   const ContextModel* _emModel;
+   const TransitionModel* _transModel;
+
+   // length of alignment for convenience
+   size_t _N;
+
+   // keep pointer to alignment
+   const std::pair<std::string, std::string>* _alignment;
+
+   // the current detected state along the alingment;
+   // we always keep track of a non-context (0) and context state (1)
+   // in the case of match, we set them both to match
+   FTAGStates::State _state[2];
+
+   // if the above state is an indel, we also store its size
+   size_t _indelSize;
+
+   // the previous state (before above)
+   FTAGStates::State _lastState[2];
+
+   // viterbi dynamic programming table.  stores value for 
+   // 0 = non-context, 1 = context.  
+   double _table[2];
+   // like above but for previous column
+   double _lastTable[2];
+   
 };
+
+inline FTAGStates::State FTAGScore::getContext(FTAGStates::State state) const
+{
+  if (state == FTAGStates::S_Dx)
+  {
+    return FTAGStates::S_MDx;
+  }
+  else if (state == FTAGStates::S_Ix)
+  {
+    return FTAGStates::S_MIx;
+  }
+  return state;
+}
+
+template<typename T>
+inline void FTAGScore::copy2(T a[2], T b[2]) const
+{
+  a[0] = b[0];
+  a[1] = b[1];
+}
+
+template<typename T>
+inline void FTAGScore::init2(T a[2], T b0, T b1) const
+{
+  a[0] = b0;
+  a[1] = b1;
+}
 
 #endif
