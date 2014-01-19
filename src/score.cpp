f48b1372218fd5364e46b01988b427f14c32d0c8
Glenn Hickey <glenn.hickey@gmail.com>
2014-01-19 15:22:20 -0500

    fill in score tool

--- src/score.cpp
+++ src/score.cpp
@@ -10,17 +10,15 @@
 #include <algorithm>
 #include <iterator>
 
-#include "ftagtrain.h"
+#include "ftagscore.h"
 #include "ftagargs.h"
 #include "pairs.h"
+#include "axtreader.h"
 
 using namespace std;
 using namespace FTAGTableStructs;
 using namespace FTAGStates;
 
-static void writePosterior(size_t num, const FTAGModel& ftag, 
-									const PairAlignment& alignment,
-                           const deque<Trace>& trace, ostream& postfile);
 
 int main(int argc, char** argv)
 {
@@ -28,9 +26,8 @@ int main(int argc, char** argv)
    {
       FTAGArgs args;
       args.getArgs(argc, argv);
-      srand48(args._seed);
-      cout << "SEED=" << args._seed << endl;
 
+      // Load the model parameters
       FTAGParams params;
       ifstream pfile(args._fpFile.c_str());
       if (!pfile)
@@ -43,66 +40,35 @@ int main(int argc, char** argv)
       params.setSingleF84(args._singleF84);
       params.setDoubleF84(args._doubleF84);
 
-      ifstream ifile(args._inFile.c_str());
-      if (!ifile)
-      {
-         cerr << "Specify input sequences with infile=<path>" << endl;
-         exit(1);
-      }
-   
-      ofstream ofile(args._outFile.c_str());
-      if (!ofile)
-      {
-         cerr << "Specify output file with outfile=<path>" << endl;
-         exit(1);
-      }
-
-      ofstream postfile;
-      if (args._outFile2.empty() == false)
-      {
-         postfile.open(args._outFile2.c_str());
-      }
-
-      Pairs inputPairs, outputPairs;
-      ifile >> inputPairs;
-
-      cout << "input file contains " << inputPairs.size() << " pairs" << endl;
-   
+      // Create the model
       ContextModel em;
       TransitionModel tm;
       em.setSingleAndDouble(params);
       tm.setSimplified(params);
+      FTAGModel ftag;
+      ftag.setTransitionModel(tm);
+      ftag.setEmissionModel(em);
 
-      double totalVll = 0.;
-   
-      for (size_t i = 0; i < inputPairs.size(); ++i)
+      // Create the scorer
+      FTAGScore tagScore(&ftag);
+
+      // Load the AXT alignments into memory (note that this is 
+      // pretty wasteful since we only need to parse one at a time, but
+      // am working with the existing classes...)
+      AxtReader ar;
+      ar.read(args._inFile);
+      const vector<AxtPair>& wholeThing = ar.getAlignments();
+      
+      // Score each alignment
+      double totalScore = 0.0;
+      for (size_t i = 0; i < wholeThing.size(); ++i)
       {
-         FTAGModel ftag;
-         ftag.setSequences(inputPairs[i].first, inputPairs[i].second,
-                           args._winSize, args._winSize);
-
-         ftag.setTransitionModel(tm);
-         ftag.setEmissionModel(em);
-         double vll = log(ftag.viterbi());
-
-         deque<Trace> trace;
-         string a, b;
-         ftag.viterbiTrace(a, b, trace);
-
-         outputPairs.push_back(PairAlignment(a, b));
-
-         if (postfile.is_open())
-         {
-            ftag.forward();
-            ftag.backward();
-            writePosterior(i, ftag, outputPairs.back(), trace, postfile);
-         }
-
-         totalVll += vll;
+        const pair<string, string>& alignment = wholeThing[i].getAlignment();
+        double score = tagScore.logViterbiScore(alignment);
+        totalScore += score;
       }
 
-      ofile << outputPairs;
-      cout << "Total Viterbi Probability = " << totalVll << endl;
+      cout << totalScore << endl;
    
       return 0;
    }
@@ -111,56 +77,4 @@ int main(int argc, char** argv)
       cout << "ERROR: " << message << endl;
       return -1;
    }
-}
-
-void writePosterior(size_t num, const FTAGModel& ftag, 
-                    const PairAlignment& alignment,
-                    const deque<Trace>& trace, ostream& postfile)
-{
-   size_t pos = 0;
-   vector<pair<double, Trace> > posts(alignment.first.length(), 
-                                      pair<double, Trace>(0., Trace()));
-   double lprob = 0;
-   size_t i, j;
-   State s;
-
-   postfile << num << ")" << endl;
-
-   for (i = 0; i < trace.size(); ++i)
-   {
-      s = trace[i]._s;
-      if (s == S_Mx || s == S_Dx || s == S_Ix)
-      {
-         lprob = log(ftag.posterior(trace[i]));
-         posts[pos].first = lprob;
-         posts[pos].second = trace[i];
-         ++pos;
-      }
-      else if (s == S_MDx || s == S_MIx)
-      {
-         size_t len = s == S_MDx ? trace[i]._x3 - trace[i]._x1 :
-            trace[i]._x4 - trace[i]._x2;
-         assert ((s == S_MDx && trace[i]._x2 == trace[i]._x3) ||
-                 (s == S_MIx && trace[i]._x3 == trace[i]._x4));
-
-         for (j = 0; j < len; ++j)
-         {
-            lprob = log(ftag.posterior(trace[i+j]));
-            posts[pos + j].first = lprob;
-            posts[pos + j + len].first = lprob;
-            posts[pos + j].second = trace[i+j];
-            posts[pos + j + len].second = trace[i+j];
-         } 
-         pos = pos + 2 * len;
-         i = i + len; // also cover close state
-      }
-   }
-   
-   for (size_t i = 0; i < posts.size(); ++i)
-   {
-      postfile << i << " " << posts[i].first 
-					<< " " << posts[i].second << endl;
-   }
-   
-   postfile << endl;
 }
